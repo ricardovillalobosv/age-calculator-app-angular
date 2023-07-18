67511cfbf836da430aaa9526953be82e8a44a33c
Ricardo Villalobos Vargas <ricardo_villalobosv@hotmail.com>
2023-07-18 12:14:40 -0500

    ADD components

---
+++ src/app/age-calculator/age-calculator.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-age-calculator',
+  templateUrl: './age-calculator.component.html',
+  styleUrls: ['./age-calculator.component.scss']
+})
+export class AgeCalculatorComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InputComponent } from './components/input/input.component';
+import { InputErrorComponent } from './components/input-error/input-error.component';
+import { ButtonComponent } from './components/button/button.component';
+import { ResultComponent } from './components/result/result.component';
+import { AgeCalculatorComponent } from './age-calculator/age-calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent,
+    InputErrorComponent,
+    ButtonComponent,
+    ResultComponent,
+    AgeCalculatorComponent
   ],
   imports: [
     BrowserModule
